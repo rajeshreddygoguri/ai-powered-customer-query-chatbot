43a7c4d4bbc255d1f6c297cef83a740007a04bc6
rajeshreddygoguri <145776295+rajeshreddygoguri@users.noreply.github.com>
2025-04-05 14:53:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cookieparser())
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173'  
+    origin: 'https://client-pdhx.onrender.com'  
 }));
 
 app.use('/api/auth',AuthRoutes)
